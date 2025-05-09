fec51e45fa3b7e4ed01ccf85497cfd6ca7826178
srkabane <153277153+srkabane@users.noreply.github.com>
2025-05-09 09:16:58 +0530

    Fix ESLint TypeScript configuration

--- calc-fe-main/eslint.config.js
+++ calc-fe-main/eslint.config.js
@@ -1,18 +1,20 @@
-import js from '@eslint/js'
-import globals from 'globals'
-import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh'
-import react from 'eslint-plugin-react'
-import tseslint from 'typescript-eslint'
+import js from '@eslint/js';
+import globals from 'globals';
+import reactHooks from 'eslint-plugin-react-hooks';
+import reactRefresh from 'eslint-plugin-react-refresh';
+import react from 'eslint-plugin-react';
+import * as tseslint from '@typescript-eslint/eslint-plugin';
+import tsParser from '@typescript-eslint/parser';
 
 export default [
     js.configs.recommended,
-    ...tseslint.configs.recommended,
     {
         files: ['**/*.{ts,tsx}'],
         ignores: ['dist'],
         languageOptions: {
+            parser: tsParser,
             ecmaVersion: 2020,
+            sourceType: 'module',
             globals: {
                 ...globals.browser
             },
@@ -23,11 +25,13 @@ export default [
             }
         },
         plugins: {
+            '@typescript-eslint': tseslint,
             'react': react,
             'react-hooks': reactHooks,
             'react-refresh': reactRefresh,
         },
         rules: {
+            ...tseslint.configs.recommended.rules,
             ...reactHooks.configs.recommended.rules,
             'react-refresh/only-export-components': [
                 'warn',
